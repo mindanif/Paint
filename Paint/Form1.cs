b9a015bf516237d5cab6a95ce63aba4240541880
Раиль Гизатуллин <RaiRGizatullin@stud.kpfu.ru>
2023-02-26 12:57:37 +0300

    Новая версия ( Rail )

--- Paint/Form1.cs
+++ Paint/Form1.cs
@@ -18,7 +18,8 @@ namespace Paint
             InitializeComponent();
             Rectangle rectangle = Screen.PrimaryScreen.Bounds;
             map = new Bitmap(rectangle.Width, rectangle.Height);
-            graphics = Graphics.FromImage(map); phoneColor = Color.White;
+            graphics = Graphics.FromImage(map); 
+            phoneColor = Color.White;
             p = new Painter(gfs);
         }
 
@@ -79,72 +80,7 @@ namespace Paint
                 }
             }
         }
-        private void LoadImage(string fileName)
-        {
-            try
-            {
-                // Çàãðóæàåì èçîáðàæåíèå èç ôàéëà
-                Image image = Image.FromFile(fileName);
-
-                // Îòîáðàæàåì èçîáðàæåíèå â PictureBox
-                pictureBox1.Image = image;
-
-                // Îáíîâëÿåì òåêóùèé Bitmap äëÿ ðèñîâàíèÿ ïîâåðõ çàãðóæåííîãî èçîáðàæåíèÿ
-                map = new Bitmap(image);
-
-                // Îáíîâëÿåì Graphics îáúåêò äëÿ íîâîãî Bitmap
-                graphics = Graphics.FromImage(map);
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void ñîõðàíèòüToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            using (SaveFileDialog dialog = new SaveFileDialog())
-            {
-                dialog.Filter = "JPEG files (*.jpeg;*.jpg)|*.jpeg;";
-
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    // Ñîçäàåì íîâîå èçîáðàæåíèå ñ âûáðàííûì ôîíîì
-                    Bitmap bmp = new Bitmap(map.Width, map.Height);
-                    using (Graphics g = Graphics.FromImage(bmp))
-                    {
-                        g.Clear(phoneColor);
-                        g.DrawImage(map, 0, 0);
-                    }
-
-                    // Ñîõðàíÿåì íîâîå èçîáðàæåíèå â ôîðìàòå JPEG
-                    bmp.Save(dialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
-                }
-            }
-        }
-
-        private void çàãðóçèòüToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-            using (OpenFileDialog dialog = new OpenFileDialog())
-            {
-                dialog.Filter = "JPEG files (*.jpeg;*.jpg)|*.jpeg;";
-
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    // Çàãðóæàåì èçîáðàæåíèå èç ôàéëà
-                    LoadImage(dialog.FileName);
-                }
-            }
-
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private Figure CreateFigure()//ñîçäàíà ñ öåëüþ öåíòðàëèçàöèè ãåíåðàöèè îáúåêòîâ
+                private Figure CreateFigure()//ñîçäàíà ñ öåëüþ öåíòðàëèçàöèè ãåíåðàöèè îáúåêòîâ
         {
             if (radioButton1.Checked)
             {
@@ -165,21 +101,6 @@ namespace Paint
         {
             p.Paint(e.Graphics);
         }
-
-        private void phonecolorselect_Click(object sender, EventArgs e)
-        {
-            using (ColorDialog dialog = new ColorDialog())
-            {
-                if (dialog.ShowDialog() == DialogResult.OK)
-                {
-                    // Óñòàíàâëèâàåì öâåò ôîíà PictureBox â âûáðàííûé öâåò
-                    pictureBox1.BackColor = dialog.Color;
-
-                    // Ñîõðàíÿåì âûáðàííûé öâåò â ïåðåìåííóþ
-                    phoneColor = dialog.Color;
-                }
-            }
-        }
         private void LoadImage(string fileName)
         {
             try
@@ -203,7 +124,7 @@ namespace Paint
             }
         }
 
-        private void ñîõðàíèòüToolStripMenuItem_Click(object sender, EventArgs e)
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
             using (SaveFileDialog dialog = new SaveFileDialog())
             {
@@ -213,10 +134,14 @@ namespace Paint
                 {
                     // Ñîçäàåì íîâîå èçîáðàæåíèå ñ âûáðàííûì ôîíîì
                     Bitmap bmp = new Bitmap(map.Width, map.Height);
+                    pictureBox1.Refresh();
+                    pictureBox1.DrawToBitmap(map, pictureBox1.ClientRectangle);
+
                     using (Graphics g = Graphics.FromImage(bmp))
                     {
                         g.Clear(phoneColor);
                         g.DrawImage(map, 0, 0);
+
                     }
 
                     // Ñîõðàíÿåì íîâîå èçîáðàæåíèå â ôîðìàòå JPEG
@@ -225,7 +150,7 @@ namespace Paint
             }
         }
 
-        private void çàãðóçèòüToolStripMenuItem_Click(object sender, EventArgs e)
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
         {
             using (OpenFileDialog dialog = new OpenFileDialog())
             {
@@ -237,6 +162,14 @@ namespace Paint
                     LoadImage(dialog.FileName);
                 }
             }
+
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
